41c500c92f41bc0a9c41b4bb9aabf0bba62cc093
Hee Hwang <heehcs@gmail.com>
2022-01-12 16:15:14 -0500

    implement library.cc

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# protobuf_example
+## Create a cpp class
+
+```bash
+protoc -I=. --cpp_out=. ./simpleResults.proto
+protoc -I=. --java_out=. ./simpleResults.proto
+g++ -c -o library.o library.cc -I"${JAVA_HOME}/include"
+
+```
+
---
+++ library.cc
@@ -0,0 +1,35 @@
+// #include <cstring>
+#include <jni.h>
+// #include <cinttypes>
+#include "simpleResults.pb.h"
+
+extern "C" JNIEXPORT jobject JNICALL
+Java_com_securityandsafetythings_examples_helloworld_rest_HelloWorldEndpoint_createSR(JNIEnv *env, jclass clazz) {
+
+  // Dummy input
+  filebenchmark::SimpleResults* SR = new filebenchmark::SimpleResults();
+  SR->set_status(2);
+  SR->set_inf_time_ms(100);
+
+  // Get the class we wish to return an instance of
+  jclass srclass = env->FindClass("com/securityandsafetythings/libraries/filebenchmark/SimpleResultsOuterClass");
+
+  // Get the method id of an empty constructor in clazz
+  jmethodID constructor = env->GetMethodID(srclass, "<init>", "()V");
+
+  // Create an instance of clazz
+  jobject obj = env->NewObject(srclass, constructor);
+
+  // Get Field references
+  jfieldID param1Field = env->GetFieldID(srclass, "status_", "F");
+  jfieldID param2Field = env->GetFieldID(srclass, "infTimeMs_", "F");
+
+  // Set fields for object
+  env->SetIntField(obj, param1Field, SR->status());
+  env->SetIntField(obj, param2Field, SR->inf_time_ms());
+
+  delete SR;
+  
+  // return object
+  return obj;
+}
